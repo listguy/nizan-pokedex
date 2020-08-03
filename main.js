bcb8d57769384ed405f21e441c695e1654468cdf
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-08-03 14:33:39 +0300

    third task

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
-const searchPokemon = async (pokemonId = 3) => {
+let pokemonId;// declaring new variable pokemonId that stores the id of the pokemon we need to get
+const searchButton = document.querySelector('#searchButon');// getting the search field from index
+const searchField = document.querySelector('#search');// getting the search button from index
+
+searchButton.addEventListener('click', async () => { 
+  pokemonId = searchField.value || 7;
   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  console.log(data)
-};
-searchPokemon();
+  const pHeight = data.height;
+  const pName = data.name;
+  const pImage = data.sprites.front_default;
+  InsertDataToHtml([pName, pHeight, pImage]);
+  console.log(`name: ${pName}, height: ${pHeight}, image-url: ${pImage}`);
+  //console.log(data)
+});
+
+function InsertDataToHtml(data) {
+  let para = document.querySelector('p');
+  para.innerText = `Pokemon name: ${data[0]}, Pokemon height: ${data[1]},`;
+  let image = document.createElement('img');
+  image.src = data[2];
+  para.append(image);
+}
+// const searchPokemon = async (pokemonId) => { 
+//   const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+//   const pHeight = data.height;
+//   const pName = data.name;
+//   console.log(`name: ${pName}, height: ${pHeight}`);
+//   //console.log(data)
+// };
 
