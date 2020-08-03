a7b51349f8b1f3b763f4b4e26b7e01e79e104644
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-08-03 19:07:22 +0300

    last task done

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ const searchButton = document.querySelector('#searchButon');// getting the searc
 const searchField = document.querySelector('#search');// getting the search button from index
 const err404Img = document.querySelector('#err404img');
 
-searchButton.addEventListener('click', async () => { 
+searchButton.addEventListener('click',  () => { 
   pokemonId = searchField.value || 7;
   err404Img.hidden = true;
+  UpdatePreviewPokemon(pokemonId)
+});
+
+// gets and updates pokemon data from api to html the page
+async function UpdatePreviewPokemon(pID) {
   try {
-    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pID}`);
     const chars = { // object that contains all the pokemon characteristics
       pName: data.name,
       pHeight: data.height,
@@ -17,17 +22,17 @@ searchButton.addEventListener('click', async () => {
       pTypes: data.types
     };
     InsertDataToHtml(chars);
-    // Array.from(chars.pTypes).forEach(type => )
   } catch (e) {
       console.log(e);
       err404Img.hidden = false;
   }
-});
 
+}
+// inserts pokemon data to index page
 function InsertDataToHtml(data) {
   let para = document.querySelector('p');
   console.log(data.pTypes[0].type.name);
-  // let types = data.pTypes.reduce((type1, type2) => `${type1} ${type2.type.name},`, '');
+
   // creating a clickable span tag for every type name
   para.innerText = `Pokemon name: ${data.pName}, Pokemon height: ${data.pHeight}, Pokemon weight: ${data.pWeight}, Pokemon types: `;
   Array.from(data.pTypes).forEach(tObj => {
@@ -46,14 +51,18 @@ function InsertDataToHtml(data) {
 
 // creates a list consisting all pokemon of given type
 async function CreateListOfType(type) {
-  const { data } = await axios.get(`https://pokeapi.co/api/v2/type/${type}`); //sends a request to get all pokemons of given type
+  const { data } = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);// sends a request to get all pokemons of given type
   document.querySelector('#typeList')? document.querySelector('#typeList').remove() : "";
   let h2 = document.createElement('h2');
   h2.innerText = `${type} Pokemons`;
   let ul = document.createElement('ul');
   data.pokemon.forEach(obj => {
     let li = document.createElement('li');
-    li.innerText = obj.pokemon.name;
+    let span = document.createElement('span');
+    span.innerText = obj.pokemon.name;
+    // adding an event listener for each pokemon in the list to preview his data when it's name is clicked
+    span.addEventListener('click', (e) => UpdatePreviewPokemon(e.target.innerText));
+    li.append(span);
     ul.append(li);
   })
   let container = document.createElement('div');
