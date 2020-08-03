c676956531dd7facc2aa3a0d778283d4deb33eed
Nitzan Listman <67858069+listguy@users.noreply.github.com>
2020-08-03 14:57:23 +0300

    404 error handle

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let pokemonId;// declaring new variable pokemonId that stores the id of the pokemon we need to get
 const searchButton = document.querySelector('#searchButon');// getting the search field from index
 const searchField = document.querySelector('#search');// getting the search button from index
+const err404Img = document.querySelector('#err404img');
 
 searchButton.addEventListener('click', async () => { 
   pokemonId = searchField.value || 7;
-  const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
-  const pHeight = data.height;
-  const pName = data.name;
-  const pSpriteFront = data.sprites.front_default;
-  const pSpriteBack = data.sprites.back_default;
-  InsertDataToHtml([pName, pHeight, pSpriteFront, pSpriteBack]);
+  err404Img.hidden = true;
+  try {
+    const { data } = await axios.get(`http://pokeapi.co/api/v2/pokemon/${pokemonId}`);
+    const pHeight = data.height;
+    const pName = data.name;
+    const pSpriteFront = data.sprites.front_default;
+    const pSpriteBack = data.sprites.back_default;
+    InsertDataToHtml([pName, pHeight, pSpriteFront, pSpriteBack]);
+  } catch (e) {
+      console.log("not found!" + e.message);
+      err404Img.hidden = false;
+  }
   //console.log(`name: ${pName}, height: ${pHeight}, image-url: ${pSpriteFront}`);
   //console.log(data)
 });
